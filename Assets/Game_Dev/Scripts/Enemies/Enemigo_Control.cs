1645f5dc5e4225d5c07fa96f1ef83bb487baf9ee
LejoSan <alejandrosandovalp@outlook.com>
2024-10-29 19:45:57 +0100

    Cambio de script

--- Assets/Game_Dev/Scripts/Enemies/Enemigo_Control.cs
+++ Assets/Game_Dev/Scripts/Enemies/Enemigo_Control.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Enemigo_Control : MonoBehaviour
 {
+
     [SerializeField] private int vida = 5;
     [SerializeField] private float velocidadMovimiento = 3f;
     [SerializeField] private GameObject indicador;
@@ -11,14 +12,25 @@ public class Enemigo_Control : MonoBehaviour
     private bool jugadorEnRango = false;
     private bool siendoComido = false;
     private Rigidbody rbEnemigo;
+    private bool enContactoParaDano = false;
+
+    private Rana_Control rana;
+    private Rigidbody rbjugador;
 
     public int Vida { get => vida; set => vida = value; }
     public bool SiendoComido { get => siendoComido; set => siendoComido = value; }
+    public bool EnContactoParaDano { get => enContactoParaDano; set => enContactoParaDano = value; }
 
+
+    // Start is called before the first frame update
     void Start()
     {
         jugador = GameObject.FindGameObjectWithTag("Jugador").transform;
         rbEnemigo = GetComponent<Rigidbody>();
+
+
+        rana = jugador.GetComponent<Rana_Control>();
+        rbjugador = jugador.GetComponent<Rigidbody>();
 
         if (indicador != null)
         {
@@ -26,15 +38,18 @@ public class Enemigo_Control : MonoBehaviour
         }
     }
 
- 
+    // Update is called once per frame
     void Update()
     {
-        if (jugadorEnRango && !siendoComido)
+        if (jugadorEnRango && !EnContactoParaDano)
         {
             SeguirJugador();
         }
+        //if (jugadorEnRango && !siendoComido)
+        //{
+        //    SeguirJugador();
+        //}
     }
-
     private void SeguirJugador()
     {
         Vector3 direccion = (jugador.position - transform.position).normalized;
@@ -59,21 +74,58 @@ public class Enemigo_Control : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Jugador"))
+        {
+            EnContactoParaDano = true;
+            jugadorEnRango = false; // Detenemos el movimiento hacia el jugador
+        }
+    }
+
     public void RecibirDahno(int cantidad)
     {
-        vida -= cantidad;
-
-        if (siendoComido)
+        if (EnContactoParaDano) // Aplica daño solo si está en contacto y siendo comido
         {
-            rbEnemigo.constraints = RigidbodyConstraints.FreezePosition;
+            vida -= cantidad;
+
+            rbjugador.constraints = RigidbodyConstraints.FreezeAll;
+
+            rbEnemigo.constraints = RigidbodyConstraints.FreezeAll; // Congela el movimiento al ser atacado
+            Debug.Log("Enemigo recibió " + cantidad + " de daño. Vida actual: " + vida);
+
+            if (vida <= 0)
+            {
+                DestruirEnemigo();
+            }
         }
+        //vida = vida - cantidad;
+
+        //if (siendoComido)
+        //{
+        //    rbEnemigo.constraints = RigidbodyConstraints.FreezePosition;
+        //    Debug.Log("Enemigo recibió " + cantidad + " de daño. Vida actual: " + vida);
+        //}
+
+    }
+    public void CongelarEnemigo(bool congelar)
+    {
+        rbEnemigo.constraints = congelar ? RigidbodyConstraints.FreezeAll : RigidbodyConstraints.None;
     }
 
     public void DestruirEnemigo()
     {
         if (vida <= 0)
         {
+            // Llamar a LiberarMovimiento en el jugador
+            GameObject jugador = GameObject.FindGameObjectWithTag("Jugador");
+            if (jugador != null)
+            {
+                jugador.GetComponent<Rana_Control>().LiberarMovimiento();
+            }
+
             Destroy(gameObject);
+            Debug.Log("Enemigo destruido y movimiento del jugador liberado.");
         }
     }
 
